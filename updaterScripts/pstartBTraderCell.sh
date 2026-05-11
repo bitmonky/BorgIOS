a72a22d11344ce2166a9f70af68c6e013adb05f4
Guerrilla Coders Inc Headquarters <81542700+bitmonky@users.noreply.github.com>
2026-05-11 12:08:51 -0400

    Add In Bin Stream Object

--- updaterScripts/pstartBTraderCell.sh
+++ updaterScripts/pstartBTraderCell.sh
@@ -18,11 +18,13 @@ curl -s https://admin.bitmonky.com/bitMDis/ptreeReceptorObj.js  -o ptreeReceptor
 curl -s https://admin.bitmonky.com/bitMDis/btraderOrganObj.js   -o btraderOrganObj.js
 curl -s https://admin.bitmonky.com/bitMDis/btraderOrganCell.js  -o btraderOrganCell.js
 curl -s https://admin.bitmonky.com/bitMDis/peerTree.js          -o peerTree.js
+curl -s https://admin.bitmonky.com/bitMDis/DStreamMgrObj.js     -o DStreamMgrObj.js
 curl -s https://admin.bitmonky.com/bitMDis/peerCrypt.js         -o peerCrypt.js
 curl -s https://admin.bitmonky.com/bitMDis/mkyDatef.js          -o mkyDatef.js
 curl -s https://admin.bitmonky.com/bitMDis/networkWebConsole.js -o networkWebConsole.js
 curl -s https://admin.bitmonky.com/bitMDis/bitWebMoniter.js     -o bitWebMoniter.js
 curl -s https://admin.bitmonky.com/bitMDis/pstartBTraderCell.sh -o pstartBTraderCell.sh
+curl -s https://admin.bitmonky.com/bitMDis/btraderDB.js            -o btraderDB.js
 
 echo "CronoTreeCell files updated!"
 echo "Run with: pm2 start cronoTreeCell.js"
